d981ff30cae3b177aa2cbf923cd651d0ada0de4e
norihirosunada <norisuna1201@gmail.com>
2020-02-15 07:56:45 +0000

    018, done

--- app.rb
+++ app.rb
@@ -11,3 +11,21 @@ get '/' do
     erb :index
 end
 
+post '/answers' do
+    Answer.create(
+        name: params[:name],
+        age: params[:age],
+        gender: params[:gender],
+        grade: params[:grade],
+        email: params[:email],
+        password: params[:password],
+        date: params[:date],
+        content: params[:content]
+    )
+    redirect '/'
+end
+
+get '/answers' do
+    @answers = Answer.all
+    erb :answers
+end
--- models.rb
+++ models.rb
@@ -4,5 +4,13 @@ Bundler.require
 ActiveRecord::Base.establish_connection(ENV['DATABASE_URL']||"sqlite3:db/development.db")
 
 class Answer < ActiveRecord::Base
-
+    has_secure_password
+    validates :name,
+        presence: true
+    validates :email,
+        presence: true,
+        format: {with: /[@]/}
+    validates :password,
+        presence: true,
+        length: {in: 6..10}
 end
